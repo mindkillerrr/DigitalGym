ef4f2e8a8509396675ab977082b513490e55865e
mindkillerrr <1730652477@qq.com>
2021-04-19 23:17:26 +0800

    wd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
     //
     public static void main(String[] args) throws IOException {
         //IO.main(args);
-        //launch(args);
+        launch(args);
 
 
     }
---
+++ src/main/java/ViewController/TrainerMainSceneController.java
@@ -0,0 +1,13 @@
+package ViewController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class TrainerMainSceneController {
+    public ChoiceBox myLiveFilterType;
+    public Button Goto;
+
+    public void GotoLiveDetail(ActionEvent actionEvent) {
+    }
+}
